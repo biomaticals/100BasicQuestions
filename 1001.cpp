1b2db66aeb97fbea20010195cb4122ef33a21ca6
biomaticals <biomaticals@naver.com>
2021-01-22 14:53:40 +0900

    1002 soloved

--- 1001.cpp
+++ 1001.cpp
@@ -6,4 +6,4 @@ int main(void)
 {
 	printf("Hello\n");
 	return 0;
-}
+} 
---
+++ 1002.cpp
@@ -0,0 +1,9 @@
+//https://codeup.kr/problem.php?id=1002
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World\n");
+    return 0;
+}
