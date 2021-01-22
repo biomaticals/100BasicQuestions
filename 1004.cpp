0dcd45a9f478437c03a7a9a68c61d7cd2a1bdc27
biomaticals <biomaticals@naver.com>
2021-01-22 22:19:56 +0900

    solved 1004, 1005

---
+++ 1004.cpp
@@ -0,0 +1,9 @@
+//https://codeup.kr/problem.php?id=1004
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\'Hello\'");
+    return 0;
+}
---
+++ 1005.cpp
@@ -0,0 +1,9 @@
+//https://codeup.kr/problem.php?id=1005
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\"Hello World\"\n");
+    return 0;
+}
