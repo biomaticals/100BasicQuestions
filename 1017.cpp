9eba265f2ecf09ad6860d8b5c4d4fb39172c7eb7
biomaticals <biomaticals@naver.com>
2021-01-25 20:46:26 +0900

    1017 solved

---
+++ 1017.cpp
@@ -0,0 +1,11 @@
+//https://codeup.kr/problem.php?id=1017
+
+#include <stdio.h>
+
+int main(void)
+{
+    int nData{};
+    scanf("%d",&nData);
+    printf("%d %d %d",nData,nData,nData);
+    return 0;
+}
