ec54fd3ac069977d69b0e99a23098240b0fedb1a
biomaticals <biomaticals@naver.com>
2021-01-26 01:48:44 +0900

    1028 solved

---
+++ 1028.cpp
@@ -0,0 +1,11 @@
+//https://codeup.kr/problem.php?id=1028
+
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int unData{};
+    scanf("%u",&unData);
+    printf("%u\n",unData);
+    return 0;
+}
