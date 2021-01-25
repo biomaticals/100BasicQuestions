cd6c9eb106e66f456d69c3dc95c1345271156290
biomaticals <biomaticals@naver.com>
2021-01-26 01:34:07 +0900

    1026 solved

---
+++ 1026.cpp
@@ -0,0 +1,13 @@
+//https://codeup.kr/problem.php?id=1026
+
+#include <stdio.h>
+
+int main(void)
+{
+    int nHour{};
+    int nMinute{};
+    int nSecond{};
+    scanf("%2d:%2d:%2d",&nHour,&nMinute,&nSecond);
+    printf("%d",nMinute);
+    return 0;
+}
