608ba6e9c68a07b45d8615bad3aa0d2d064c0bf4
biomaticals <biomaticals@naver.com>
2021-01-25 23:40:25 +0900

    1021 solved

---
+++ 1021.cpp
@@ -0,0 +1,11 @@
+//https://codeup.kr/problem.php?id=1021
+
+#include <stdio.h>
+
+int main(void)
+{
+    char ar_cData[51]{};
+    scanf("%s",ar_cData);
+    printf("%s",ar_cData);
+    return 0;
+}
