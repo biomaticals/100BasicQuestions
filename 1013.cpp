e119701b008dac72df8fd2436b0c700bd4b97b67
biomaticals <biomaticals@naver.com>
2021-01-25 20:30:39 +0900

    1013 solved

---
+++ 1013.cpp
@@ -0,0 +1,12 @@
+//https://codeup.kr/problem.php?id=1013
+
+#include <stdio.h>
+
+int main(void)
+{
+    int nData1{};
+    int nData2{};
+    scanf("%d%d",&nData1,&nData2);
+    printf("%d %d",nData1,nData2);
+    return 0;
+}
