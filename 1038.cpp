af7f3c95c52ef39ad61f25fa8a519f10d39b10bf
biomaticals <biomaticals@naver.com>
2021-01-26 02:38:32 +0900

    1038 solved

---
+++ 1038.cpp
@@ -0,0 +1,11 @@
+//https://codeup.kr/problem.php?id=1038
+
+#include<stdio.h>
+
+int main(void)
+{
+    long long int nData1{};
+    long long int nData2{};
+    scanf("%lld %lld",&nData1,&nData2);
+    printf("%lld",nData1+nData2);
+}
