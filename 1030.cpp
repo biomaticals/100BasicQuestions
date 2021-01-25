4998bbf5658591aba326ba9715d7a46d1cd13170
biomaticals <biomaticals@naver.com>
2021-01-26 02:01:24 +0900

    1030 solved

---
+++ 1030.cpp
@@ -0,0 +1,11 @@
+//https://codeup.kr/problem.php?id=1030
+
+#include <stdio.h>
+
+int main(void)
+{
+    long long int nData{};
+    scanf("%lld",&nData);
+    printf("%lld",nData);
+    return 0;
+}
