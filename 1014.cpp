08ff29537138069e9de8c3c5785be1f5277fef56
biomaticals <biomaticals@naver.com>
2021-01-25 20:37:14 +0900

    1014 solved

---
+++ 1014.cpp
@@ -0,0 +1,12 @@
+//https://codeup.kr/problem.php?id=1014
+
+#include <stdio.h>
+
+int main(void)
+{
+    char cData1{};
+    char cData2{};
+    scanf("%c %c",&cData1,&cData2);
+    printf("%c %c",cData2,cData1);
+    return 0;
+}
