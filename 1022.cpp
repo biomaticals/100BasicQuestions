f7666e59862a622d52d430c4f89675695613c696
biomaticals <biomaticals@naver.com>
2021-01-26 00:32:43 +0900

    1022 solved

---
+++ 1022.cpp
@@ -0,0 +1,11 @@
+//https://codeup.kr/problem.php?id=1022
+
+#include <stdio.h>
+
+int main(void)
+{
+   char ar_cData[2001];
+   fgets(ar_cData,2000,stdin);
+   printf("%s",ar_cData);
+   return 0;
+}
