380dd7c7360d6cdf85f143795af4fdaf1acf6eec
biomaticals <biomaticals@naver.com>
2021-01-28 10:12:50 +0900

    1077 solved

---
+++ 1077.cpp
@@ -0,0 +1,14 @@
+//https://codeup.kr/problem.php?id=1077
+
+#include <stdio.h>
+
+int main(void)
+{
+    int nData{};
+    scanf("%d", &nData);
+    for (int i = 0; i <= nData; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
